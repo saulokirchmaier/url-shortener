b1b44dc803da3b1f55e070051dae02b22a15a653
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2024-08-10 17:41:37 -0300

    AuthGuard

--- src/auth/guards/auth-guard.ts
+++ src/auth/guards/auth-guard.ts
@@ -6,14 +6,26 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from '../auth.service';
+import { Reflector } from '@nestjs/core';
+import { Role } from './role.decorator';
+import { RoleType } from 'src/utils/role.enum';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get(Role, context.getHandler());
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
+
+    if (!token && roles.context === RoleType.NEW_URL) {
+      request.user = false;
+      return true;
+    }
 
     if (!token) {
       throw new UnauthorizedException('Token not provided');
